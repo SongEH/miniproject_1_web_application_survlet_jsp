837b7791be56bd89ff25eb3bcc06a17e6865ba55
14A <14A@192.168.219.120>
2024-06-14 15:39:23 +0900

    15:40수정

--- miniproject1/src/main/java/dao/BlogDao.java
+++ miniproject1/src/main/java/dao/BlogDao.java
@@ -1,16 +1,43 @@
 package dao;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import vo.MemberVo;
 
 public class BlogDao {
 	
+	static BlogDao single = null;
+
+	public static BlogDao getinstance() {
+
+		if (single == null)
+			single = new BlogDao();
+
+		return single;
+	}
+
+	private BlogDao() {
+	}
+	
 	public boolean login(int m_idx, String m_name, String m_id, String m_pw, String m_email, String m_intro, String m_rdate, String m_mdate) {
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
 		boolean status = false;
+		
 		MemberVo mv = new MemberVo();
-		String sql = "select * from member where m_id = ? and m_pw ?";
-//		PreparedStatement psm = mv.conn.prepareStatement(sql);
+		try {
+			String sql = "select * from member where m_id = ? and m_pw ?";
+			PreparedStatement pstm = conn.prepareStatement(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 		
