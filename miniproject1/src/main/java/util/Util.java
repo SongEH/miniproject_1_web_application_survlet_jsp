c6bddd58af9fe493d8c0b84eb5d77bc936f3e6f3
14A <14A@192.168.219.120>
2024-06-14 17:02:47 +0900

    보안성 유틸 코드 작성

--- miniproject1/src/main/java/util/Util.java
+++ miniproject1/src/main/java/util/Util.java
@@ -1,16 +1,11 @@
 package util;
 
-import java.io.IOException;
-
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import vo.MemberVo;
-
 public class Util {
 	
-	public final static String base_url = "";
+	public final static String base_url = "http://localhost:8080/";
 	
+	// 입력된 문자를 MD5 해시로 변환하는 코드 
+	// 비밀번호 등 해시코드로 변환시켜 보안성 높이는 용도
 	public static String MD5(String md5) {
         try {
             java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
@@ -24,45 +19,5 @@ public class Util {
         }
         return null;
     }
-
-    public MemberVo isLogin(HttpServletRequest request, HttpServletResponse response) {
-
-        if (request.getCookies() != null ) {
-            Cookie[] cookies = request.getCookies();
-            for ( Cookie cookie : cookies ) {
-                if ( cookie.getName().equals("user") ) {
-                    String vales = cookie.getValue();
-                    try {
-                        String[] arr = vales.split("_");
-                        request.getSession().setAttribute("aid", Integer.parseInt(arr[0]) );
-                        request.getSession().setAttribute("name",arr[1] + " " + arr[2] );
-                    } catch (NumberFormatException e) {
-                        Cookie cookie1 = new Cookie("user", "");
-                        cookie1.setMaxAge(0);
-                        response.addCookie(cookie1);
-                    }
-                    break;
-                }
-            }
-        }
-
-
-        Object sessionObj = request.getSession().getAttribute("aid");
-        MemberVo mem = new MemberVo();
-        if ( sessionObj == null ) {
-            try {
-                response.sendRedirect(base_url);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }else{
-            int aid = (int) request.getSession().getAttribute("aid");
-            String name = (String) request.getSession().getAttribute("ad");
-            mem.setM_type(aid);
-            mem.setM_pw(name);
-        }
-        return mem;
-    }
-	
 
 }
