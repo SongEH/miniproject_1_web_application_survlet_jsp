f1fa5c33427536f808d8deeaf1f8ca6366953ff3
SongEH <will9512@naver.com>
2024-06-15 15:47:33 +0900

    XSS(크로스 사이트 스크립팅) 방어 기능 추가

--- miniproject1/src/main/java/util/Util.java
+++ miniproject1/src/main/java/util/Util.java
@@ -1,8 +1,23 @@
 package util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Util {
 	
 	public final static String base_url = "http://localhost:8080/miniproject1";
+	
+	private static final Map<String, String> HTML_ESCAPE_MAP;
+
+    static {
+        HTML_ESCAPE_MAP = new HashMap<>();
+        HTML_ESCAPE_MAP.put("&", "&amp;");
+        HTML_ESCAPE_MAP.put("<", "&lt;");
+        HTML_ESCAPE_MAP.put(">", "&gt;");
+        HTML_ESCAPE_MAP.put("\"", "&quot;");
+        HTML_ESCAPE_MAP.put("'", "&#x27;");
+        HTML_ESCAPE_MAP.put("/", "&#x2F;");
+    }
 	
 	// 입력된 문자를 MD5형식의 해시 코드로 변환하는 코드 
 	// 비밀번호 등 해시코드로 변환시켜 보안성 높이는 용도
@@ -18,6 +33,25 @@ public class Util {
         } catch (java.security.NoSuchAlgorithmException e) {
         }
         return null;
+    }
+	
+    // HTML 이스케이프 처리
+	 /**
+     * HTML 특수 문자를 이스케이프 처리합니다.
+     * 
+     * @param input 원본 문자열
+     * @return 이스케이프 처리된 문자열
+     */
+    public static String escapeHtml(String input) {
+        if (input == null) return null;
+
+        StringBuilder escaped = new StringBuilder();
+        for (char c : input.toCharArray()) {
+            String replacement = HTML_ESCAPE_MAP.get(String.valueOf(c));
+            escaped.append(replacement != null ? replacement : c);
+        }
+
+        return escaped.toString();
     }
 
 }
