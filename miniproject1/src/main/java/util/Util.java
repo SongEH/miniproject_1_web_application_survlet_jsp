2efc758d9347a9052af8d6a0ba4bf9320ad18caa
14A <14A@192.168.219.120>
2024-06-17 13:26:07 +0900

    필요없는 코드 삭제

--- miniproject1/src/main/java/util/Util.java
+++ miniproject1/src/main/java/util/Util.java
@@ -2,8 +2,6 @@ package util;
 
 public class Util {
 	
-//	public final static String base_url = "http://localhost:8080/miniproject1";
-		
 	// 입력된 문자를 MD5형식의 해시 코드로 변환, 보안성 높이는 용도
     // MD5형식은 보안이 다른 것보다 취약하나 미니프로젝트이기때문에 암호화했다는 것만 담기 위해 사용
     // 대체로 보안성 좋은 SHA3-?을 사용하거나 랜덤으로 salt를 붙여 사용하거나 pepper를 사용하거나 bcrypt,argon2를 사용가능(뒤로 갈수록 보안성 높음) 
