7566c7d04ef7ee338f8d7e26da84158b2da45409
14A <14A@192.168.219.70>
2024-06-21 17:10:53 +0900

    게시글 목록보기,쓰기,삭제

---
+++ miniproject1/src/main/java/action/PostInsertFormAction.java
@@ -0,0 +1,33 @@
+package action;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class PostInsertFormAction
+ */
+
+@WebServlet("/post/insert_form.do")
+public class PostInsertFormAction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		//Dispatcher형식으로 호출
+		String forward_page = "post_insert_form.jsp";
+		RequestDispatcher disp = request.getRequestDispatcher(forward_page);
+		disp.forward(request, response);
+
+	}
+
+}
--- miniproject1/src/main/java/service/DBService.java
+++ miniproject1/src/main/java/service/DBService.java
@@ -1,43 +1,66 @@
 package service;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 public class DBService {
 
-	static {
-
-		try {
-			Class.forName("oracle.jdbc.OracleDriver");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-
-	}
-
+	// single-ton pattern : 객체 1개만 생성해서 이용하자
 	static DBService single = null;
 
-	public static DBService getinstance() {
+	DataSource ds = null;
+	
+	static public DBService getInstance() {
 
+		//없으면 생성해라
 		if (single == null)
 			single = new DBService();
 
 		return single;
 	}
 
+	// 외부에서 객체생성하지 말아라...
 	private DBService() {
-	}
 
+		
+		try {
+			//JNDI을 이용해서 DataSource정보를 얻어온다
+			//1.InitialContext생성(JNDI -> interface 추출 객체)
+			InitialContext ic = new InitialContext();
+			
+			//2.Context 정보 얻어온다 -> context는 javax.naming / context는 object이므로 cast해야 함
+			Context context = (Context) ic.lookup("java:comp/env");
+			
+			//3.naming을 이용해서 DataSource를 얻어온다
+			ds = (DataSource) context.lookup("jdbc/oracle_test");
+			
+			//2번과 3번을 한번에
+			//ds = (DataSource) ic.lookup("java:comp/env/jdbc/oracle_test");
+			
+			
+			
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}//end:DBService()
+	
+	
 	public Connection getConnection() throws SQLException {
-		String url = "jdbc:oracle:thin:@localhost:1521:xe";
-		String user = "test";
-		String pwd = "test";
-
-		Connection conn = DriverManager.getConnection(url, user, pwd);
-
-		return conn;
+		
+		//DataSource을 이용해서 BasicDataSource가 관리하고 있는 컨넥션을 요청
+		return ds.getConnection();
 	}
-
+	
+	
+	
+	
+	
+	
+	
 }
-
